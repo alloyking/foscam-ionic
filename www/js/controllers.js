a813def624f7f2f672493121acfb30de46e2df78
Tim Shultis <tim.shultis@morsekode.com>
2016-08-13 22:20:23 -0500

    remove pointless notes and update readme

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,28 +5,19 @@ angular.module('starter.controllers', [])
 	//what we load on the initail view
 	CameraConfigLoader.get('camera-one-config.json').then(function() {
 		// camera is loaded and displayed
-
-		// I want IR on when I arrive at the camera
-		//$scope.command_url = CameraUrlBuilder.doCommand('ir_on');
 	});
 
 	//what happens when we change slides
 	$scope.slideHasChanged = function(index) {
 		if (index == 0) {
-			
+
 			CameraConfigLoader.get('camera-one-config.json').then(function() {
-				// camera is loaded and displayed
-				
-				// I want IR on when I arrive at the camera
-				//$scope.command_url = CameraUrlBuilder.doCommand('ir_on');
+				// camera  1 is loaded and displayed
 			});
 
 		} else {
 			CameraConfigLoader.get('camera-two-config.json').then(function() {
-				// camera is loaded and displayed
-
-				// I want IR on when I arrive at the camera
-				//$scope.command_url = CameraUrlBuilder.doCommand('ir_on');
+				// camera 2 is loaded and displayed
 			});
 		}
 	};
